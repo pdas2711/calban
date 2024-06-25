70848e3c792161b219d4212fe327e9057a99bd23
Pial Das <pdas2711@proton.me>
2024-06-25 14:53:44 -0400

    Fixed imports after folder restructure.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,10 @@ import {BrowserRouter as Router, Routes, Route, Redirect} from "react-router-dom
 
 import logo from './logo.svg';
 import './App.css';
-import LoginPage from "./components/Login/pages/Login.js";
-import Dashboard from "./components/Dashboard/Dashboard.js";
+import LoginPage from "./components/pages/Login/Login.js";
+import Dashboard from "./components/pages/Dashboard/Dashboard.js";
+import Account from "./components/pages/Dashboard/pages/Account/Account.js";
+import ChangePass from "./components/pages/Dashboard/pages/Account/pages/ChangePass/ChangePass.js";
 
 function App() {
 	return (
@@ -13,6 +15,8 @@ function App() {
 				<Routes>
 					<Route path="/" element={<LoginPage/>}/>
 					<Route path="/dashboard" element={<Dashboard/>}/>
+					<Route path="/account" element={<Account/>}/>
+					<Route path="/changepass" element={<ChangePass/>}/>
 				</Routes>
 			</main>
 		</Router>
