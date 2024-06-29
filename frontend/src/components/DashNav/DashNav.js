801eb74c1471fb0c3f64e5f67ec6494e278b7071
Pial Das <pdas2711@proton.me>
2024-06-28 21:07:33 -0400

    Moved 'pages' to root directory and fixed NavLink for Project link to point to Dashboard in DashNav.

--- frontend/src/components/DashNav/DashNav.js
+++ frontend/src/components/DashNav/DashNav.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
-import Account from "../pages/Dashboard/pages/Account/Account.js";
+import Dashboard from "../../pages/Dashboard/Dashboard.js";
+import Account from "../../pages/Dashboard/pages/Account/Account.js";
 
 const DashNav = (props) => {
 	return <ul className="main-header">
-		<li><NavLink to="/dashboard">Projects</NavLink></li>
+		<li><NavLink to="/dashboard" element={<Dashboard/>}>Projects</NavLink></li>
 		<li><NavLink to="/account" element={<Account/>}>Account</NavLink></li>
 		</ul>
 };
