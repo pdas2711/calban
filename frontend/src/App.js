082cd96e7809843c50d23d74469e2e22541e27b0
Pial Das <pdas2711@proton.me>
2024-06-28 21:33:20 -0400

    Fixed import statements after moving 'pages' and adding new route for adding a board.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,12 @@ import {BrowserRouter as Router, Routes, Route, Redirect} from "react-router-dom
 
 import logo from './logo.svg';
 import './App.css';
-import LoginPage from "./components/pages/Login/Login.js";
-import Dashboard from "./components/pages/Dashboard/Dashboard.js";
+import LoginPage from "./pages/Login/Login.js";
+import Dashboard from "./pages/Dashboard/Dashboard.js";
 import DashNav from "./components/DashNav/DashNav.js";
-import Account from "./components/pages/Dashboard/pages/Account/Account.js";
-import ChangePass from "./components/pages/Dashboard/pages/Account/pages/ChangePass/ChangePass.js";
+import Account from "./pages/Dashboard/pages/Account/Account.js";
+import ChangePass from "./pages/Dashboard/pages/Account/pages/ChangePass/ChangePass.js";
+import NewBoard from "./pages/Dashboard/pages/NewBoard/NewBoard.js";
 
 function App() {
 	return (
@@ -19,6 +20,7 @@ function App() {
 					<Route path="/dashboard" element={<Dashboard/>}/>
 					<Route path="/account" element={<Account/>}/>
 					<Route path="/changepass" element={<ChangePass/>}/>
+					<Route path="/new_board" element={<NewBoard/>}/>
 				</Routes>
 			</main>
 		</Router>
