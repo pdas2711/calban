aea3f79dbd01ed15f8313b6aeaefbf244bab05e8
Pial Das <pdas2711@proton.me>
2024-06-28 21:20:26 -0400

    Added New Board page.

---
+++ frontend/src/pages/Dashboard/pages/NewBoard/NewBoard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NewBoard = props => {
+	const [boardName, setBoardName] = useState();
+	const [description, setDescription] = useState():
+	const addBoardHandler = event => {
+	};
+	return (
+		<div>
+			<form className="new-board" onSubmit={addBoardHandler}>
+				Board Name: <input className="add-board-textbox" type="text" value={boardName}/><br/>
+				Description: <input className="add-board-textbox" type="text" value={description}/><br/><br/>
+				<button type="submit">Add Board</button>
+			</form>
+		</div>
+	);
+};
+
+export default NewBoard;
