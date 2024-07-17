b0a514a967fee620e6d7e8cdc762a299e3c212a4
Pial Das <pdas2711@proton.me>
2024-07-16 22:49:10 -0400

    Changed route from POST to GET and added route for getting tasks.

--- web-server/routes/dash-route.js
+++ web-server/routes/dash-route.js
@@ -3,7 +3,9 @@ const dashController = require('../controllers/dash-controller');
 
 const router = express.Router();
 
-router.post('/get_boards', dashController.getBoards);
+router.get('/get_tasks', dashController.getTasks);
+router.get('/get_boards', dashController.getBoards);
+
 router.post('/promote_task', dashController.promoteTask);
 router.post('/demote_task', dashController.demoteTask);
 
