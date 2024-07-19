77807054639f3ac7eaa956b7d632123c44b5319f
Pial Das <pdas2711@proton.me>
2024-07-18 20:52:53 -0400

    Added new request method to create a new task.

--- web-server/controllers/caldav-relay.js
+++ web-server/controllers/caldav-relay.js
@@ -1,5 +1,6 @@
-const {XMLValidator, XMLParser} = require('fast-xml-parser');
+const { XMLValidator, XMLParser } = require('fast-xml-parser');
 const http = require('http');
+const { v4: uuidv4 } = require('uuid');
 
 function getBoardsXML(request) {
 	return new Promise(function(resolve, reject) {
@@ -59,7 +60,6 @@ function getTasksVCalendar(request) {
 				'Depth': 'infinity',
 				'Authorization': auth
 			}
-			//body: '<d:propfind xmlns:d="DAV:" xmlns:cs="https://calendarserver.org/ns/"><d:prop><d:resourcetype /><d:displayname /></d:prop></d:propfind>'
 		};
 		let tasksRetrieved;
 		http.request(reqOptions, res => {
@@ -78,5 +78,36 @@ function getTasksVCalendar(request) {
 	});
 }
 
+function addTaskVCalendar(request) {
+	return new Promise(function(resolve, reject) {
+		const username = request.username;
+		const password = request.password;
+		const userCalendarId = request.calendarId;
+		const auth = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
+		const reqOptions = {
+			host: 'localhost',
+			port: 5233,
+			path: '/' + username,
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/xml',
+				'Depth': 'infinity',
+				'Authorization': auth
+			}
+		};
+		const newTask = "BEGIN:VTODO\n" +
+			"CREATED:" + request.task.created + "\n" +
+			"DTSTAMP:" + request.task.dtstamp + "\n" +
+			"LAST-MODIFIED:" + request.task.lastMod + "\n" +
+			"PERCENT-COMPLETE:" + request.task.percentComp + "\n" +
+			"SUMMARY:" + request.task.summary + "\n" +
+			"UID:" + uuidv4() + "\n";
+		http.request(reqOptions, res => {
+			res.on('data', chunk => {});
+		}).write(newTask).end();
+	});
+}
+
 exports.getBoardsXML = getBoardsXML;
 exports.getTasksVCalendar = getTasksVCalendar;
+exports.addTaskVCalendar = addTaskVCalendar;
