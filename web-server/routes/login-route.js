c1e74e7da9db2f310ab1876980fe715fe6395eaa
Pial Das <pdas2711@proton.me>
2024-07-19 20:43:20 -0400

    Added better naming for routes.

--- web-server/routes/login-route.js
+++ web-server/routes/login-route.js
@@ -3,6 +3,7 @@ const loginController = require('../controllers/login-controller');
 
 const router = express.Router();
 
-router.post('/', loginController.login);
+router.post('/user', loginController.login);
+router.post('/get_caldav', loginController.getServerCred);
 
 module.exports = router;
